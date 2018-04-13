049565f4592a624bca54c3778e076635d3b07416
ryosuke_ishiji <ishijiryosuke@MacBook-Air-4.local>
2018-04-13 17:34:54 +0900

    Add Transition destination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def after_sign_out_path_for(resource)
+  '/users/sign_in' # サインアウト後のリダイレクト先URL
+  end
+
 end
