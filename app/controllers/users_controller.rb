df04a2f5dc19239c536163db22699cf79f5888c4
hurueru <tech-camp@12-010tech-camp-EXPtaikenkai1-A-no-MacBook-Air.local>
2018-04-19 17:37:23 +0900

    add user_edit_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
   def show
+    # @screenname = user.name
     @name = current_user.name
-    # @screenname
     # @tweets = Tweet.where(user_id: current_user.id)
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+    @users = User.find(params[:id])
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
