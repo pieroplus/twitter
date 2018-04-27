e818aee8190cc1b9c669e8fdddb7a7ec81e64fd3
ryosuke_ishiji <ishijiryosuke@MacBook-Air-4.local>
2018-04-27 13:53:20 +0900

    add icon setting function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name],keys:[:icon_image],keys:[:header_image])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name], keys: [:screen_name])
   end
 
