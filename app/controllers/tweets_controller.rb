75d2578fed030edb0af8806447f70a21a9d352cd
yuji005 <yuji0816@gmail.com>
2018-04-27 20:09:11 +0900

    削除修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,7 +8,6 @@ class TweetsController < ApplicationController
     @tweets = Tweet.where(user_id: @indicate).order("created_at DESC")
     @users = current_user
     @recommends = User.where.not(id: @follow).where.not(id: current_user.id)
-
   end
 
   def create
@@ -27,14 +26,14 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
+    params.require(:tweets).permit(
+      :text,
+      :user_id,
+      images_attributes: [:content])
   end
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
-    # params.require(:tweets).permit(
-    #   :text,
-    #   :user_id,
-    #   images_attributes: [:content])
   end
 
   def user_params
