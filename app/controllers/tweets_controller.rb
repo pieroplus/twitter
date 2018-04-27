b448c07c23b1599bc33d66a2de168c783e000f55
ryosuke_ishiji <ishijiryosuke@MacBook-Air-4.local>
2018-04-27 11:32:43 +0900

    fix some bugs

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,12 +2,14 @@ class TweetsController < ApplicationController
 
   before_action :move_to_index, except: :index
   before_action :get_followuser, only: :index
+
   def index
     @tweet = Tweet.new
     @tweet.images.build
     @tweets = Tweet.where(user_id: @indicate).order("created_at DESC")
     @users = current_user
     @recommends = User.where.not(id: @follow).where.not(id: current_user.id)
+    @tweetnumber = Tweet.where(user_id: current_user.id).count(:user_id)
   end
 
   def create
@@ -18,14 +20,14 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-  end
-
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
     params.require(:tweet).permit(
       :text,
       :user_id,
       images_attributes: [:content])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
   def user_params
